d495356c8cbe9f02a89c687b299b3222a5b60a23
Neil Sawhney <Neil@neil-sawhney.com>
2026-01-16 13:54:30 -0500

    cleanup:

--- woonuxt_base/app/components/shopElements/WebsiteShortDescription.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-const { description } = useAppConfig();
-</script>
-
-<template>
-  <p class="mt-4 text-sm text-gray-700 dark:text-gray-300 xl:max-w-md">{{ description }}</p>
-</template>
